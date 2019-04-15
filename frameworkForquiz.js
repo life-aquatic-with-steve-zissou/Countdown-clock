81668d1dab97f8503f8f087c1efc3555fd110fad
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-15 16:48:15 -0500

    added some animation

--- frameworkForquiz.js
+++ frameworkForquiz.js
@@ -1,14 +1,14 @@
-//this is the function for the countdown clock
-var user = prompt("Enter your name");
-var playerName = document.getElementById('player');
-playerName.innerHTML = user;
-
+//PlayerName feature below commented out for now
+//var user = prompt("Enter your name");
+//var playerName = document.getElementById('player');
+//playerName.innerHTML = user;
+//restart variable and function below
 var resetbtn = document.getElementById('restart');
 
 resetbtn.addEventListener('click',function () {
     location.reload();
 },false);
-
+//this is the function for the countdown clock
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -44,6 +44,11 @@ onclick = function () {
         display = document.querySelector('#time');
     startTimer(twoMinutes, display);
 };
+
+
+
+
+
 
 
 
